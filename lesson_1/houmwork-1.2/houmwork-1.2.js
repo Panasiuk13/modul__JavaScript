2f7ce458a4c86590d45f13baabcdeeaac99fdd62
Panasiuk13 <anna.panasiuk01@gmail.com>
2021-07-21 20:11:31 +0300

    lesson2

--- lesson_1/houmwork-1.2/houmwork-1.2.js
+++ lesson_1/houmwork-1.2/houmwork-1.2.js
@@ -1,8 +1,7 @@
 let x = 11;
 let y = 23;
 
-let y1 = y - x - 1;
-let x1 = x + 12;
+y = [x, x = y][0];
+x = [y, y = x][0];
 
-
-console.log(y1, x1);
+console.log(y, x);
---
+++ lesson_2/lesson_2.js
@@ -0,0 +1,98 @@
+/*var a = alert ("WArning!");
+var p = prompt(" Number");
+var c = confirm( "yes or no")
+
+console.log(a, p, c);*/
+
+
+/*console.log(1 > 2 || 1 < 2);
+console.log(1 > 3 || 1 < 2);
+
+
+console.log(2 > 4 && 2 < 10);
+console.log(4 > 2 && 2 < 10);
+
+console.log(!true);
+console.log(!false);
+
+
+var year = 1999;
+
+if (1990 > year){
+    console.log('Go work!')
+}
+
+console.log(' условие закончилось')*/
+
+
+/*var year = 2002;
+
+if (2000 > year){
+    console.log('Go work!')
+} else{
+    console.log('Go to school!')
+}*/
+
+/*var year = 2010;
+
+if (2000 > year && 1999 < year){
+    console.log('Go to school!')
+} else if(1990 > year){
+    console.log('Go work!')
+}else{
+    console.log('Go childhood!')
+}*/
+
+
+/*var year = 2002;
+var test = 1900 > year ? console.log('Go work!') : console.log('Go to school!');*/
+
+/*var x = 2 + 2;
+
+switch (x) {
+    case 1:
+        console.log('1');
+        break;
+    case 2:
+        console.log('2');
+        break;
+    case 3:
+        console.log('3');
+        break;
+    case 4:
+        console.log('4');
+        break;
+    case 5:
+        console.log('5');
+        break;
+}
+*/
+
+/*let x = 0;
+
+while (x <= 10){
+    console.log(x);
+    x++;
+}*/
+
+/*let x = 10;
+
+do{
+console.log(x);
+x --;
+}
+while (x > 0);*/
+
+/*for (var x = 0; x < 10; x++){
+    console.log(x);
+}*/
+
+/*for (var x = 0; x < 10; x++){
+    if(x == 5){
+        console.log('it is 5');
+        //break;
+        continue;
+    }
+    console.log('it is' + x);
+}*/
+
