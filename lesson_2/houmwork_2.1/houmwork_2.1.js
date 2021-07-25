af952c7566f2d649c94153be37879ce153148c31
Panasiuk13 <anna.panasiuk01@gmail.com>
2021-07-25 14:13:04 +0300

    hw

---
+++ lesson_2/houmwork_2.1/houmwork_2.1.js
@@ -0,0 +1,24 @@
+var p = 20;
+var year = prompt("Укажите вораст");
+if (year > 20){
+    console.log(year);
+}
+
+var p = 20;
+var year = prompt("Укажите вораст");
+var test = 20 < year ? console.log(year) : console.log(p);
+
+
+var x = 1 + 1;
+switch (x) {
+    case 1:
+    case 2:
+    case 3:
+        console.log(alert('ok!'));
+        break;
+    case 4:
+    case 5:
+        console.log(alert('hi!'));
+        break;
+}
+
---
+++ lesson_2/houmwork_2.2/houmwork_2.2.js
@@ -0,0 +1,14 @@
+var x = 0;
+
+while (x = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 +13) {
+    console.log(x);
+    x ++;
+}
+
+
+var x = 0;
+for ( x = 0; x >= 0; x ++){
+   console.log(x);
+}
+
+
