5b0767821e0f38262b32235d74f504d248431eb8
Panasiuk13 <anna.panasiuk01@gmail.com>
2021-08-01 13:43:35 +0300

    hw4

---
+++ lesson_4/houmwork_4/houmwork_4.js
@@ -0,0 +1,31 @@
+/*let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let arr1 = [];
+for (let i = 0; i < arr.length; i++){
+    if(arr[i] % 2 === 0){
+      arr1.push(arr[i]);
+    }
+}
+console.log(arr1);*/
+
+
+//-------2----///
+/*let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+for (let i = 0; i < arr.length; i++){
+    if(arr[i] ===)
+}*/
+
+
+//-------3------///
+let x = prompt('Number');
+while (prompt()){
+        console.log(x);
+    }
+    x ++;
+let arr = [2, 14, 65];
+let largest = 0;
+for (let i = 0; i < arr.length; i++) {
+    if (largest < arr[i]) {
+        largest = arr[i]
+    }
+}
+console.log(largest);
