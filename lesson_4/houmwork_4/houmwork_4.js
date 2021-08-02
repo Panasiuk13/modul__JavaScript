7ee55cb4588afc6a75f3abf022cadae2f83de356
Panasiuk13 <anna.panasiuk01@gmail.com>
2021-08-02 15:02:22 +0300

    hw4

--- lesson_4/houmwork_4/houmwork_4.js
+++ lesson_4/houmwork_4/houmwork_4.js
@@ -13,7 +13,7 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
 let arr1 = [];
 let sum = 0;
 for (let i = 0; i < arr.length; i++){
-    arr1.push(arr[i]*(i*5));
+    arr1.push((i*5));
 }
 
 console.log(arr1);
