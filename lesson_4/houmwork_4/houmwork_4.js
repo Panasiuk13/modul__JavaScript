a063cbfa62bc3082967b2a46a372907d5d505173
Panasiuk13 <anna.panasiuk01@gmail.com>
2021-08-02 19:40:30 +0300

    hw4

--- lesson_4/houmwork_4/houmwork_4.js
+++ lesson_4/houmwork_4/houmwork_4.js
@@ -11,7 +11,6 @@ console.log(arr1);*/
 //-------2----///
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 let arr1 = [];
-let sum = 0;
 for (let i = 0; i < arr.length; i++){
     arr1.push((i*5));
 }
---
+++ lesson_5/lesson_5.js
@@ -0,0 +1,72 @@
+/*let passport = {
+    number: "mc111111",
+    name : "Anais",
+    surname : "Specimen",
+    birthday : "1954-07-12"
+    };
+
+/*let obj = {};
+
+console.log(passport.number);
+
+let name =passport.name;
+console.log(name);
+
+let surname = passport["surname"];
+console.log(surname);
+
+
+
+obj.name = "Neo";
+
+console.log(obj);
+
+
+car.name = "Audi";
+car.color = "red";
+car.age = "1990";
+
+console.log("name" in car);
+console.log("Headlights" in car);
+
+console.log(car.Headlights);*/
+
+
+/*let country = {
+    name : "America",
+    "10count car" : 99999,
+
+};
+console.log(country["10count car"]);
+
+console.log(car);
+
+
+for (let key in car){
+    console.log(key + ": " + car[key]);
+}*/
+
+let person = {
+    name: "Lui",
+    age: "23"
+};
+
+/*let newPerson = person;
+
+person.age = "45"
+
+console.log(person, newPerson);
+
+
+let personCountry = {};
+
+for (let key in person){
+    personCountry[key] = person[key];
+}
+person.name = "Dima";
+console.log(person,personCountry );*/
+
+
+let city = [
+    {name: "Minsk" population: 1000000}
+]
